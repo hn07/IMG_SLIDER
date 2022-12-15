3fa75b2216cd879d8adcab16a74fcad9f9dd18bd
Nguyễn Huỳnh Hữu Nhâm <nhamb2005809@student.ctu.edu.vn>
2022-12-15 09:30:08 +0700

    first

---
+++ JavaScript.js
@@ -0,0 +1,10 @@
+document.getElementById('left').onclick = function() {
+    let lists = document.querySelectorAll('.item');
+    document.getElementById('slide').appendChild(lists[0]);
+    
+}
+document.getElementById('right').onclick = function() {
+    let lists = document.querySelectorAll('.item');
+    document.getElementById('slide').prepend(lists[lists.length -1]);
+    
+}
